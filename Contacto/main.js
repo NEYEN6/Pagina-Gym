6a58083a1d2b79cecdebe6e2dca5099025b75c34
Sebastián Neyen Frezzini <sebasfrezzini@gmail.com>
2021-10-29 01:50:56 -0300

    Se agrega el Protecyo

---
+++ Contacto/main.js
@@ -0,0 +1,68 @@
+const body = document.querySelector("body");
+const navbar = document.querySelector(".navbar");
+const menuBtn = document.querySelector(".menu-btn");
+const cancelBtn = document.querySelector(".cancel-btn");
+menuBtn.onclick = () => {
+    navbar.classList.add("show");
+    menuBtn.classList.add("hide");
+    body.classList.add("disabled");
+}
+cancelBtn.onclick = () => {
+    body.classList.remove("disabled");
+    navbar.classList.remove("show");
+    menuBtn.classList.remove("hide");
+}
+window.onscroll = () => {
+    this.scrollY > 20 ? navbar.classList.add("sticky") : navbar.classList.remove("sticky");
+}
+
+$(document).ready(function() {
+
+    $('#btnSend').click(function() {
+
+        var errores = '';
+
+        // Validado Nombre ==============================
+        if ($('#names').val() == '') {
+            errores += '<p>Escriba un nombre</p>';
+            $('#names').css("border-bottom-color", "#F14B4B")
+        } else {
+            $('#names').css("border-bottom-color", "#d1d1d1")
+        }
+
+        // Validado Correo ==============================
+        if ($('#email').val() == '') {
+            errores += '<p>Ingrese un correo</p>';
+            $('#email').css("border-bottom-color", "#F14B4B")
+        } else {
+            $('#email').css("border-bottom-color", "#d1d1d1")
+        }
+
+        // Validado Mensaje ==============================
+        if ($('#mensaje').val() == '') {
+            errores += '<p>Escriba un mensaje</p>';
+            $('#mensaje').css("border-bottom-color", "#F14B4B")
+        } else {
+            $('#mensaje').css("border-bottom-color", "#d1d1d1")
+        }
+
+        // ENVIANDO MENSAJE ============================
+        if (errores == '' == false) {
+            var mensajeModal = '<div class="modal_wrap">' +
+                '<div class="mensaje_modal">' +
+                '<h3>Error al ingresar los datos</h3>' +
+                errores +
+                '<span id="btnClose">Cerrar</span>' +
+                '</div>' +
+                '</div>'
+
+            $('body').append(mensajeModal);
+        }
+
+        // CERRANDO MODAL ==============================
+        $('#btnClose').click(function() {
+            $('.modal_wrap').remove();
+        });
+    });
+
+});
